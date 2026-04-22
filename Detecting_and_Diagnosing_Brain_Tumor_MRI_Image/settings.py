d446fe548c4597a547e6197031e037d0829ccec3
Sneha Kolla <your-email@gmail.com>
2026-04-22 12:22:49 +0530

    Debugging Render: Enabled DEBUG and simplified static storage

--- Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
+++ Detecting_and_Diagnosing_Brain_Tumor_MRI_Image/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wa7ghuntm30^@4-0imff)&#+@n*%!_i29_r!m6+azr7!%w8$st'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # In production, replace with your actual domain
 
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "Assets/Static",
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
